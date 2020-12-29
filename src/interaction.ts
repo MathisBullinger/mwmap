44ee0a62ad964a5800fff4ee62f16b954da68022
Mathis Bullinger <mathis@bullinger.dev>
2020-12-29 18:06:46 +0100

    drag & zoom to target

--- src/interaction.ts
+++ src/interaction.ts
@@ -6,8 +6,10 @@ canvas.addEventListener('wheel', (e) => {
     const dZ = 1 + e.deltaY / 200
     vp.w *= dZ
     vp.h *= dZ
-    vp.x += (vp.w / dZ - vp.w) / 2
-    vp.y += (vp.h / dZ - vp.h) / 2
+    const cx = e.clientX / window.innerWidth
+    const cy = e.clientY / window.innerHeight
+    vp.x += ((vp.w / dZ - vp.w) / 2) * (cx * 2)
+    vp.y += ((vp.h / dZ - vp.h) / 2) * (cy * 2)
     startRender()
   } else {
     vp.x += e.deltaX / (1 / vp.vMin) / 1500
@@ -15,3 +17,22 @@ canvas.addEventListener('wheel', (e) => {
     startRender()
   }
 })
+
+canvas.addEventListener('mousedown', () => {
+  canvas.addEventListener('mousemove', drag)
+  canvas.addEventListener(
+    'mouseup',
+    () => {
+      canvas.removeEventListener('mousemove', drag)
+    },
+    { once: true }
+  )
+})
+
+function drag(e: MouseEvent) {
+  const dx = e.movementX / window.innerWidth
+  const dy = e.movementY / window.innerHeight
+  vp.x -= dx * vp.w
+  vp.y -= dy * vp.h
+  startRender()
+}
